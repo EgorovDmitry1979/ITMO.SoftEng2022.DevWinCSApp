0771045d5d10fcb03a676d7ff98951e4f6be9e99
EgorovDmitry1979 <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-07-06 22:17:43 +0300

    Add files via upload

---
+++ ITMO.SoftEng2022.DevWinCSApp.Lab02/02Ex3-7z/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Упражнение 2.3. Создание и использование элемента управления ToolStrip
+
+namespace MdiApplication_Lab2._3
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ParentForm());
+        }
+    }
+}
+
+// Начать с п.11
