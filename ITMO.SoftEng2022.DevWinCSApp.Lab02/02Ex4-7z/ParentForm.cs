8dc64e4328752e94ed262066b778d6580a3a5d45
EgorovDmitry1979 <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-07-06 22:18:29 +0300

    Add files via upload

---
+++ ITMO.SoftEng2022.DevWinCSApp.Lab02/02Ex4-7z/ParentForm.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MdiApplication_Lab2._4
+{
+    public partial class ParentForm : Form
+    {
+        private int openDocuments = 0; 
+        public ParentForm()
+        {
+            InitializeComponent();
+            // Свойству Text панели spData устанавливается текущая дата
+            spData.Text = Convert.ToString(System.DateTime.Today.ToLongDateString()); // п.8
+        }
+
+        private void ExitMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void WindowCascadeMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+            spWin.Text = "Windows is cascade"; // 2.4 п.7 для каскадной ориентации
+        }
+
+        private void WindowTileMenuItem_Click(object sender, EventArgs e)
+        {
+            this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+            spWin.Text = "Windows is horizontal"; // 2.4 п.7 для горизонтальной ориентации
+        }
+
+        private void NewMenuItem_Click(object sender, EventArgs e)
+        {
+            ChildForm newChild = new ChildForm();
+            newChild.MdiParent = this;
+            newChild.Show();
+            newChild.Text = newChild.Text + " " + ++openDocuments;
+        }
+
+        private void ParentForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            switch (e.ClickedItem.Tag.ToString())
+            {
+                case "NewDoc":
+                    ChildForm newChild = new ChildForm();
+                    newChild.MdiParent = this;
+                    newChild.Show();
+                    newChild.Text = newChild.Text + " " + ++openDocuments;
+                    break;
+                case "Cascade":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.Cascade);
+                    break;
+                case "Title":
+                    this.LayoutMdi(System.Windows.Forms.MdiLayout.TileHorizontal);
+                    break;
+            }
+        }
+        private void toolStrip1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ ITMO.SoftEng2022.DevWinCSApp.Lab02/02Ex4-7z/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Упражнение 2.4. Использование элемента управления StatusStrip
+
+namespace MdiApplication_Lab2._4
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new ParentForm());
+        }
+    }
+}
+
+// Начать с п.11
