e6a95dda94f3a89764a7fd894df0d7fe62d598cc
EgorovDmitry1979 <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-07-06 20:18:53 +0300

    Add files via upload

---
+++ ITMO.SoftEng2022.DevWinCSApp.Lab02/02Ex1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Упражнение 2.1. Обработка событий Click и MouseMove
+
+namespace WinQuestion_Lab2._1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
---
+++ ITMO.SoftEng2022.DevWinCSApp.Lab02/02Ex2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Упражнение 2.2. Работа со списками
+
+namespace TestList_Lab2._2
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
