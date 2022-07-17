6cf541f6be61250715887bc69c1c9f0ef8479db0
EgorovDmitry1979 <103428140+EgorovDmitry1979@users.noreply.github.com>
2022-07-17 15:45:19 +0300

    Add files via upload

---
+++ ITMO.SoftEng2022.DevWinCSWpf.Lab04/04Ex2/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UserIn2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            foreach (FontFamily F in Fonts.SystemFontFamilies) // п.12
+            {
+                comboBox1.Items.Add(F.ToString());
+            }
+        }
+
+    private void Button_Click(object sender, RoutedEventArgs e) // п.8 RTB стало подчеркивать...
+        {
+            RichTextBox.Selection.ApplyPropertyValue(FontWeightProperty, FontWeights.Bold);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e) // п.9
+        {
+            RichTextBox.Selection.ApplyPropertyValue(FontStyleProperty, FontStyles.Italic);
+        }
+
+        private void Slider1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e) // п.10
+        {
+            RichTextBox.Selection.ApplyPropertyValue(FontSizeProperty, Slider1.Value.ToString());
+        }
+
+        private void comboBox1_SelectionChanged(object sender, SelectionChangedEventArgs e) // п.11
+        {
+            RichTextBox.Selection.ApplyPropertyValue(FontFamilyProperty, new FontFamily(comboBox1.Text));
+        }
+    }
+}
+
+// НАЧИНАЕМ С П.13!!!!
---
+++ ITMO.SoftEng2022.DevWinCSWpf.Lab09/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel; // 9.1.5
+using System.Windows.Data; // 9.1.7
+using System.Windows.Media; // 9.1.7
+using System.Globalization; // 9.1.7
+
+namespace DataBindingDemo
+{
+    public class Student // 9.1.1
+    {
+        public string StudentName { get; set; } // 9.1.3
+        public bool IsEnrolled { get; set; } // 9.1.3
+    }
+
+    public class StudentList : ObservableCollection<Student> // 9.1.4 (подчеркивает <Student>)
+    {
+
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    [ValueConversion(typeof(bool), typeof(Brush))] // 9.1.8
+    public class BoolToBrushConverter: IValueConverter // п.9.1.6 + п.9.1.8
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture) // 9.1.9
+        {
+            Brush b = null;
+            if (value != null && value.GetType() == typeof(bool))
+            {
+                b = (bool)value ? Brushes.Green : Brushes.Red;
+            }
+            return b;
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) // 9.1.9
+        {
+            return null;
+        }
+    }
+}
+
